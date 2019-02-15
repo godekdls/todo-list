ba704c241a90e3cdd367c1cad87ec7d950eadc6d
dain.lee <dain.lee@navercorp.com>
2019-02-15 23:51:49 +0900

    #6 jquery rest api call

--- src/main/resources/static/test.js
+++
@@ -1,9 +0,0 @@
-$('body').on('click', '.update-btn', function(event) {
-    event.preventDefault();
-
-    var id = $(this).attr('data-id');
-    var description = $(this).attr('data-description');
-    $('#update-description').val(description);
-    $('#update-modal').modal('show')
-    var status = $(this).attr('data-status');
-});
---
+++ src/main/resources/static/update.js
@@ -0,0 +1,91 @@
+$('.status-btn').on('click', patchStatus);
+$('.update-btn').on('click', showUpdateModal);
+$('#update-submit').on('click', updateToDo);
+
+
+function patchStatus() {
+    var id = $(this).attr('data-id');
+    var status = $(this).attr('data-status');
+    if (status === 'open') {
+        status = 'closed';
+    } else {
+        status = 'open';
+    }
+    var todo = {'id': id, 'status': status};
+    $.ajax({
+        url: '/todos/' + id,
+        type: 'PATCH',
+        contentType: 'application/json;utf-8',
+        data: JSON.stringify(todo),
+        success: function () {
+            alert('처리되었습니다.');
+            $('#delete-modal').modal('hide')
+            location.reload();
+        },
+        error: function () {
+            console.log(arguments);
+            alert('처리에 실패 했습니다.');
+        }
+    });
+}
+
+function showUpdateModal() {
+    var id = $(this).attr('data-id');
+    var description = $(this).attr('data-description');
+    var status = $(this).attr('data-status');
+    var references = $(this).attr('data-references');
+    var referenceDesc = '';
+    if (references) {
+        $.each(JSON.parse(references), function (index, reference) {
+            if (index > 0) {
+                referenceDesc += ' ';
+            }
+            referenceDesc += ('@' + reference);
+        })
+    }
+    $('#update-description').val(description);
+    $('#update-references').val(referenceDesc);
+    $('#update-references').attr('data-references', references);
+    $('#update-id').val(id);
+    $('#update-status').val(status);
+    $('#update-modal').modal('show')
+}
+
+function updateToDo() {
+    var id = $('#update-id').val();
+    var description = $('#update-description').val();
+    if (!description.trim()) {
+        alert('내용을 입력해주세요');
+        return;
+    }
+    var reference = $('#update-references').val();
+    if (reference.trim()) {
+        try {
+            reference = reference.replace(/(\s*)/g, "")
+            reference = reference.split("@").filter(ref => ref).map(i => Number(i));
+        } catch (e) {
+            alert('참조 형식이 잘못되었습니다.')
+            return
+        }
+    } else {
+        reference = []
+    }
+    var status = $('#update-status').val();
+    var todo = {'id': id, 'description': description, 'references': reference, 'status': status};
+
+    $.ajax({
+        url: '/todos/' + id,
+        type: 'PUT',
+        contentType: 'application/json;utf-8',
+        data: JSON.stringify(todo),
+        success: function () {
+            alert('처리되었습니다.');
+            $('#update-modal').modal('hide')
+            location.reload();
+        },
+        error: function () {
+            console.log(arguments);
+            alert('처리에 실패 했습니다.');
+        }
+    });
+}
